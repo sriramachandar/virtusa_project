84b268b2d86560f7881d0cc69741f8eb6cc38334
sriramachandar <69522963+sriramachandar@users.noreply.github.com>
2020-08-11 21:16:23 +0530

    Add files via upload

---
+++ InternetBankingSystem/ram.sql
@@ -0,0 +1,9 @@
+CREATE TABLE CustomerDetails (
+    username VARCHAR(20),
+    password VARCHAR(20),
+    AccountType VARCHAR(20),
+    Age  VARCHAR(3),
+    MobileNumber VARCHAR(10),
+    Branch VARCHAR(20)
+);
+select * from CustomerDetails;
